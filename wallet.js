0c1bc2bd4108f42a8d09305318e9a15e9f7cf5d4
Funfart Games <funfartgames@gmail.com>
2026-04-11 15:38:24 -0400

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -1,6 +1,24 @@
 let provider;
 let signer;
 
+async function connectWallet() {
+    try {
+        // MetaMask
+        if (window.ethereum) {
+            provider = new ethers.providers.Web3Provider(window.ethereum);
+            await provider.send("eth_requestAccounts", []);
+            signer = provider.getSigner();
+
+            const address = await signer.getAddress();
+            window.userAddress = address;
+
+            document.getElementById("connectBtn").innerText =
+                address.slice(0,6) + "..." + address.slice(-4);
+
+            console.log("Connected:", address);
+            return;
+        }
+
 const CONTRACT_ADDRESS = "0x1fde0c64677C7F2E9dE23494D7bFD8caa9F63fa6";
 
 const ABI = [
